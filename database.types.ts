ebf2af6ef3f676b700e9a2a005299f2fb0081991
SupKittyMeow <supkittymeow@gmail.com>
2025-12-13 02:53:52 -0800

    OK PLEASE REVERT AAA

--- database.types.ts
+++
@@ -1,7 +0,0 @@
-[?25l[?2004h
-                                                                                                  
-  >  1. wfdcqaqihwsilzegcknq [name: GoobApp Backend, org: gzbhxmcnntavzkhetkue, region: us-east-1]
-                                                                                                  
-                                                                                                  
-    ↑/k up • ↓/j down • / filter • q quit • ? more                                                
-                                                                                                  
