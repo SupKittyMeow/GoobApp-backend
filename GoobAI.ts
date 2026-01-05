b214e3e1ce6573f3d0845d8b871623c82acf9dba
SupKittyMeow <supkittymeow@gmail.com>
2026-01-04 22:41:53 -0800

    some stuff idk

--- GoobAI.ts
+++ GoobAI.ts
@@ -1,19 +1,42 @@
+import Groq from "groq-sdk";
+
 require("dotenv").config();
 
-const client = new Groq({
-  apiKey: process.env.GROQ_API_KEY, // This is the default and can be omitted
-});
+let use_ai = true;
+let client: Groq;
 
+if (!process.env.GROQ_API_KEY) {
+  console.warn("No Groq API key found! Not using AI.");
+  use_ai = false;
+} else {
+  client = new Groq({
+    apiKey: process.env.GROQ_API_KEY, // This is the default and can be omitted
+  });
+}
 let system_prompt = "";
+let savedETag: string | null = null;
 
 async function GetSystemPrompt() {
   const url =
     "https://raw.githubusercontent.com/GoobApp/goobAI-system-prompt/refs/heads/main/prompt.txt";
   try {
-    const response = await fetch(url);
+    const headers: HeadersInit = {};
+    if (savedETag) {
+      headers["If-None-Match"] = savedETag;
+    }
+
+    const response = await fetch(url, { headers });
+
+    if (response.status === 304) {
+      return { updated: false };
+    }
+
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
+
+    savedETag = response.headers.get("ETag");
+    console.log("New system prompt! Updating old...");
 
     const result = await response.text();
     system_prompt = result;
@@ -23,6 +46,8 @@ async function GetSystemPrompt() {
 }
 
 const SendMessageToAI = async (username: string, prompt: string) => {
+  if (!use_ai) return;
+
   await GetSystemPrompt();
   const chatCompletion = await client.chat.completions.create({
     messages: [
