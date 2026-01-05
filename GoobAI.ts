0294ae892f857581c3b74fef09e494fc899dc4fb
SupKittyMeow <supkittymeow@gmail.com>
2026-01-04 22:30:50 -0800

    make GoobAI (thats official name now) prompt hosted on a different backend and fetched every time because there may be changes

--- GoobAI.ts
+++ GoobAI.ts
@@ -1,21 +1,29 @@
-import Groq from "groq-sdk";
-import { readFileSync } from "node:fs";
-
-const filePath: string = "./goofy-goober-system-prompt.txt";
-let system_prompt: string = "";
-
-try {
-  system_prompt = readFileSync(filePath, "utf-8");
-} catch (err) {
-  console.error(err);
-}
-
 require("dotenv").config();
+
 const client = new Groq({
   apiKey: process.env.GROQ_API_KEY, // This is the default and can be omitted
 });
 
+let system_prompt = "";
+
+async function GetSystemPrompt() {
+  const url =
+    "https://raw.githubusercontent.com/GoobApp/goobAI-system-prompt/refs/heads/main/prompt.txt";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.text();
+    system_prompt = result;
+  } catch (error) {
+    console.error(error instanceof Error ? error.message : String(error));
+  }
+}
+
 const SendMessageToAI = async (username: string, prompt: string) => {
+  await GetSystemPrompt();
   const chatCompletion = await client.chat.completions.create({
     messages: [
       { role: "system", content: system_prompt },
@@ -34,7 +42,6 @@ const SendMessageToAI = async (username: string, prompt: string) => {
     top_p: 1,
     stream: false,
   });
-
   return chatCompletion.choices[0].message.content;
 };
 
